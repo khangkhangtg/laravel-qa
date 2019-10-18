443a6cb746688033ea1588d5a2bee0387251ffb3
Kit Hoang <khangkhangtg@gmail.com>
2019-10-18 11:16:28 +0700

    Implement update answer

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -14,7 +14,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         // 'App\Model' => 'App\Policies\ModelPolicy',
-        'App\Question' => 'App\Policies\QuestionPolicy'
+        'App\Question' => 'App\Policies\QuestionPolicy',
+        'App\Answer' => 'App\Policies\AnswerPolicy',
     ];
 
     /**
