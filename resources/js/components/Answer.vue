db1175ca0e21d7340f9f895172f48324d0c787c5
Kit Hoang <khangkhangtg@gmail.com>
2019-11-23 23:04:48 +0700

    Beatify the flash message and confirm dialog

--- resources/js/components/Answer.vue
+++ resources/js/components/Answer.vue
@@ -31,22 +31,40 @@ export default {
             .then(res => {
                 this.bodyHtml = res.data.bodyHtml;
                 this.editing = false;
-                alert(res.data.message);
+                // alert(res.data.message);
+                this.$toast.success(res.data.message, 'Success', {timeout: 3000, position: 'topRight', progressBar: false,});
             })
             .catch(err => {
-                console.log('Something went wrong.');
+                // console.log('Something went wrong.');
+                this.$toast.error('Something went wrong.', 'Error', {timeout: 3000, position: 'topRight', progressBar: false,});
             });
         },
         
         destroy () {
-            if(confirm('Are you sure?')) {
-                axios.delete(this.endpoint)
-                    .then(res => {
-                        $(this.$el).fadeOut(500, () => {
-                            alert(res.data.message);
-                        })
-                    });
-            }
+            this.$toast.question('Are you sure?', 'Confirm',{
+                timeout: 20000,
+                close: false,
+                overlay: true,
+                displayMode: 'once',
+                zindex: 999,
+                position: 'center',
+                progressBar: false,
+                buttons: [
+                    ['<button><b>YES</b></button>', (instance, toast) => {
+                        axios.delete(this.endpoint)
+                            .then(res => {
+                                $(this.$el).fadeOut(500, () => {
+                                    this.$toast.success(res.data.message, 'Success', {timeout: 3000, position: 'topRight', progressBar: false,});
+                                    
+                                });
+                        });
+                        instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+                    }, true],
+                    ['<button>NO</button>', (instance, toast) => {
+                        instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+                    }],
+                ],
+            });
         },
     },
 
