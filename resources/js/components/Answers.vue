561b13a18f1f73edf8debd863eefa54966991e56
Kit Hoang <khangkhangtg@gmail.com>
2019-12-01 10:28:53 +0700

    Implement load more answers feature

--- resources/js/components/Answers.vue
+++ resources/js/components/Answers.vue
@@ -8,6 +8,10 @@
                     </div>
                     <hr>
                     <answer v-for="answer in answers" :answer="answer" :key="answer.id"></answer>
+
+                    <div class="text-center mt-3" v-if="nextUrl">
+                        <button class="btn btn-outline-secondary" @click.prevent="fetch(nextUrl)">Load more answers</button>
+                    </div>
                 </div>
             </div>
         </div>
@@ -18,7 +22,30 @@
 import Answer from './Answer';
 
 export default {
-    props: ['answers', 'count'],
+    props: ['question'],
+
+    data () {
+        return {
+            questionId: this.question.id,
+            count: this.question.answers_count,
+            answers: [],
+            nextUrl: null
+        }
+    },
+
+    created () {
+        this.fetch(`/questions/${this.questionId}/answers`);
+    },
+
+    methods: {
+        fetch (endpoint) {
+            axios.get(endpoint)
+                .then(({data}) => {
+                    this.answers.push(...data.data);
+                    this.nextUrl = data.next_page_url;
+                });
+        }
+    },
 
     computed: {
         title () {
--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/questions/{slug}', 'QuestionsController@show')->name('questions.sho
 
 // Route::post('/questions/{question}/answer', 'AnswerController@store')->name('answers.store');
 
-Route::resource('questions.answers', 'AnswersController')->except(['index', 'create', 'show']);
+Route::resource('questions.answers', 'AnswersController')->except(['create', 'show']);
 
 Route::post('/answers/{answer}/accept', 'AcceptAnswerController')->name('answers.accept');
 
